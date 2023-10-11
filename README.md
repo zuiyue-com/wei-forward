94699658c0b2de3baf947c87e765f13b4fce9c9e
HuangWei <market@zuiyue.com>
2023-10-11 12:03:10 +0800

    command init

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# wei frp
+# wei forward
 
 - [ ] 访问服务端获取可用 frp-server
-- [ ] open 编写配置文件，开启端口给外网访问,
-- [ ] start 每次重启，重新获取新的可用端口
+- [ ] 获取 wsl ip address
+- [ ] 启动和停止frp服务
+- [ ] 配置管理 frp 服务
+- [ ] 状态监控
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,57 @@ extern crate wei_log;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     wei_env::bin_init("wei-updater");
-    use single_instance::SingleInstance;
-    let instance = SingleInstance::new("wei-updater").unwrap();
-    if !instance.is_single() { 
-        std::process::exit(1);
-    };
+    // use single_instance::SingleInstance;
+    // let instance = SingleInstance::new("wei-updater").unwrap();
+    // if !instance.is_single() { 
+    //     std::process::exit(1);
+    // };
+
+    info!("wei-forward start");
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let mut command = "";
+
+    if args.len() > 1 {
+        command = &args[1];
+    }
+
+    match command {
+        "open" => {
+            info!("open");
+            open();
+        },
+        "start" => {
+            info!("start");
+            wei_run::command_async("wsl", vec!["/root/data/frp/frpc", "-c", "/root/data/frp/frpc.ini"])?;
+        },
+        "status" => {
+            info!("status");
+            let body: String = ureq::get("http://localhost:7400/api/status")
+                .call()?
+                .into_string()?;
+
+            println!("{}", body);
+        },
+        "stop" => {
+            info!("stop");
+            
+        },
+        _ => {
+            help();
+        }
+    }
 
     Ok(())
+}
+
+fn open() {
+
+}
+
+fn help() {
+    println!("wei-forward open <ip> <port>");
+    println!("wei-updater start");
+    println!("wei-updater stop");
 }
